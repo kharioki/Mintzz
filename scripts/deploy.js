1fee16fccaf9b430c6a24436bfac5a141cdd708a
tony <antolisah@gmail.com>
2022-04-25 10:12:25 +0300

    contract updates

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,6 +7,28 @@ async function main() {
   await mintzNFT.deployed();
 
   console.log("MintzNFT deployed to:", mintzNFT.address);
+  storeContractData(mintzNFT);
+}
+
+function storeContractData(contract) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../client/contracts";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + "/MintzNFT-address.json",
+    JSON.stringify({ MintzNFT: contract.address }, undefined, 2)
+  );
+
+  const MintzNFTArtifact = artifacts.readArtifactSync("MintzNFT");
+
+  fs.writeFileSync(
+    contractsDir + "/MintzNFT.json",
+    JSON.stringify(MintzNFTArtifact, null, 2)
+  );
 }
 
 main()
